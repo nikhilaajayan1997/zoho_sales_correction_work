1c935964db5a9071edfb0190c1a787b9b18bcf25
Nikhila Ajayan <nikhilaajayan76@gmail.com>
2023-08-10 21:03:32 +0530

    seventeenth_commit

--- zohoapp/forms.py
+++ zohoapp/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.forms import TextInput,EmailInput
 
 class EmailForm(forms.Form):
-    email = forms.EmailField()
-    subject = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'placeholder': 'Name','style': 'width: 50%;','class':'text-dark'}))
-    attach = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
-    message = forms.CharField(widget = forms.Textarea )
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email ','style': 'width: 50%;','class':'text-dark'}))
+    subject = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'placeholder': 'Subject','style': 'width: 50%;','class':'text-dark'}))
+    attach = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True,'class':'text-light'}))
+    message = forms.CharField(widget = forms.Textarea(attrs={'placeholder': 'Type messege here........','style': 'width: 50%;','class':'text-dark'}) )
